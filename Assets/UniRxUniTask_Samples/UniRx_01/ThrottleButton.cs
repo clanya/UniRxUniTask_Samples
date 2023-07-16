a65ebe6f2a7d65b213cb1a1c47e828d72c6f2693
Kazuki Tanida <tanida20rd119@gmail.com>
2023-07-16 11:28:00 +0900

    add: UniRx_01 Samples.

---
+++ Assets/UniRxUniTask_Samples/UniRx_01/ThrottleButton.cs
@@ -0,0 +1,23 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Samples.Section1
+{
+    public class ThrottleButton : MonoBehaviour
+    {
+        private void Start()
+        {
+            //Update()のタイミングでAttackボタンが押されているか判定
+            //Attackボタンが押されたらSubscribe()の処理を実行
+            //そのあと30フレームの間ボタン入力を無視する
+            this.UpdateAsObservable()
+                .Where(_ => Input.GetKeyDown(KeyCode.Space))    //わかりやすいようにSpaceにします
+                .ThrottleFirstFrame(30)
+                .Subscribe(_ =>
+                {
+                    Debug.Log("Attack!");
+                });
+        }
+    }
+}
